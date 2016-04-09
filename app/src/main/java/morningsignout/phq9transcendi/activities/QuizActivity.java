4c1bd34db4ded05cd892bbcbafe5dc3a1033fd5e
Michael Bulotano <mbulotano@gmail.com>
2016-04-08 21:40:04 -0700

    Questions updated more cleanly

--- app/src/main/java/morningsignout/phq9transcendi/activities/QuizActivity.java
+++ app/src/main/java/morningsignout/phq9transcendi/activities/QuizActivity.java
@@ -22,6 +22,8 @@ public class QuizActivity extends AppCompatActivity {
     private Button answer3; //More than half the week
     private Button answer4; //Everyday
 
+    private String[] questions;
+
     private int totalScore; //Used for answering questions
     private int scoreA;
     private int scoreB;
@@ -47,7 +49,7 @@ public class QuizActivity extends AppCompatActivity {
         answer2 = (Button) findViewById(R.id.answer2);
         answer3 = (Button) findViewById(R.id.answer3);
         answer4 = (Button) findViewById(R.id.answer4);
-
+        questions = getResources().getStringArray(R.array.questions);
         reset();
 
         //Everything is setup, start quiz
@@ -300,80 +302,64 @@ public class QuizActivity extends AppCompatActivity {
     }
 
     private void updateQuestions() {
+        question.setText(questions[questionNumber - 1]);
         switch(questionNumber) {
             //SECTION 1
             case 1:
-                question.setText("Do you find that you’ve lost a lot of interest in things used to be interested in? This can include hobbies, friends, work, food, or sex");
                 toggle = false;
                 break;
             case 2:
-                question.setText("Do you find that a lot of things are no longer enjoyable to you that you previously found enjoyable, or that you simply don’t go out and try to do enjoyable things anymore?");
                 toggle = true;
                 break;
             //SECTION 2
             case 3:
-                question.setText("Do you ever feelings of being down or or depressed?");
                 toggle = false;
                 break;
             case 4:
-                question.setText("Do you ever have feelings of hopelessness?");
                 break;
             //SECTION 3
             case 5:
-                    question.setText("Sleep is an incredibly important part of both maintaining and can also shed light on how our brains are functioning. Have you had problems with falling asleep, staying asleep, or just end up sleeping a lot less in the last two months?");
                 toggle = false;
                 break;
             case 6:
-                question.setText("How about on the other hand? Many depressed people often have the feeling of lethargy and sleeping much more than they normally do.");
                 break;
             //SECTION 4
             case 7:
-                question.setText("How has your energy level been? Do you feel tired? Drained? Feel like there is no energy to do anything?");
                 toggle = true;
                 break;
             //SECTION 5
             case 8:
-                question.setText("Someone’s appetite can be a very good indicator of when someone’s getting depressed. Have you noticed yourself eating less than you normally do?");
                 toggle = false;
                 break;
             case 9:
-                question.setText("Okay, how about a lot more than you normally do? Have you been eating lots of food at once?");
                 break;
             //SECTION 6
             case 10:
-                question.setText("A lot of people with depression have negative thoughts that are hard to control that creep in. have you had thoughts that you’re a failure?");
                 toggle = true;
                 break;
             //SECTION 7
             case 11:
-                question.setText("How’s your concentration been? Lots of people with depression have trouble concentrating on reading books or doing their homework. Have you had trouble with concentrating lately?");
                 toggle = false;
                 break;
             case 12:
-                question.setText("Have you noticed it becoming harder to finish something you started?");
                 toggle = true;
                 break;
             //SECTION 8
             case 13:
-                question.setText("Some people with depression actually feel a sense of physical slowing. have you felt a sense of physical “slowness” about yourself, like moving slowly?");
                 toggle = false;
                 break;
             case 14:
-                question.setText("Other people have said that they’ve been told they move or talk more slowly when they’re depressed. Have you had anyone comment on this lately?");
                 toggle = true;
                 break;
             //SECTION 9
             case 15:
-                question.setText("This one is really important - do you ever thinking about hurting yourself in some way, or ending your life?");
                 toggle = false;
                 break;
             case 16:
-                question.setText("How about the thought of thinking that you’d be better off dead, or that it would be great to just fall asleep and never wake up?");
                 toggle = true;
                 break;
             //RED FLAG QUESTIONS
             case 17:
-                    question.setText("In the past year have you felt depressed or sad most days even if you felt okay sometimes?");
                 redFlagQ = true;
                 toggle = true;
                 answer1.setText("Yes");
@@ -382,7 +368,6 @@ public class QuizActivity extends AppCompatActivity {
                 answer4.setVisibility(View.INVISIBLE);
                 break;
             case 18:
-                question.setText("If you are experiencing any of the problems that we asked you about, how much do these problems problems keep you from being 100% at your work, study, go about your daily business, get along with other people, and maintain your relationships?");
                 toggle = true;
                 answer1.setText("Doesn't affect me");
                 answer2.setText("A little");
@@ -392,7 +377,6 @@ public class QuizActivity extends AppCompatActivity {
                 answer4.setVisibility(View.VISIBLE);
                 break;
             case 19:
-                question.setText("Has there been a time in the past month when you have had serious thoughts about ending your life?");
                 toggle = true;
                 answer1.setText("Yes");
                 answer2.setText("No");
@@ -400,7 +384,6 @@ public class QuizActivity extends AppCompatActivity {
                 answer4.setVisibility(View.INVISIBLE);
                 break;
             case 20:
-                question.setText("Have you ever in your whole life try to kill yourself or make a suicide attempt?");
                 toggle = true;
                 answer1.setText("Yes");
                 answer2.setText("No");
