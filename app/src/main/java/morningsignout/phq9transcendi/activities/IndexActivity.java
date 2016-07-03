eac8f784237d210fc89827fab304d02cd70837f0
Iron-Sky-5 <pokeforce72@gmail.com>
2016-07-02 22:29:24 -0700

    persistence of data for firebase

--- app/src/main/java/morningsignout/phq9transcendi/activities/IndexActivity.java
+++ app/src/main/java/morningsignout/phq9transcendi/activities/IndexActivity.java
@@ -31,6 +31,7 @@ public class IndexActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
         Firebase.setAndroidContext(this);
+        Firebase.getDefaultConfig().setPersistenceEnabled(true);    // Keep sync data if app closes
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         if (getSupportActionBar() != null)
