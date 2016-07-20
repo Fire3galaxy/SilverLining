ce93bd852c095465ce2408192e20f95e7e93d124
Iron-Sky-5 <pokeforce72@gmail.com>
2016-07-19 22:43:13 -0700

    landscape for demographics intro
    slight concern with if the width of the two buttons are an issue on smaller
    phone sizes.

--- app/src/main/java/morningsignout/phq9transcendi/activities/DemographicsIntroActivity.java
+++ app/src/main/java/morningsignout/phq9transcendi/activities/DemographicsIntroActivity.java
@@ -11,14 +11,17 @@ import morningsignout.phq9transcendi.R;
  * Created by Stella on 3/20/2016.
  */
 public class DemographicsIntroActivity extends AppCompatActivity {
+    Button continue_button;
+    Button skip_button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.demographics_intro);
 
         // Contine and Skip button for Demographics
-        Button continue_button = (Button) findViewById(R.id.button_continue_demo);
-        Button skip_button = (Button) findViewById(R.id.button_skip_demo);
+        continue_button = (Button) findViewById(R.id.button_continue_demo);
+        skip_button = (Button) findViewById(R.id.button_skip_demo);
 
         // Continue - Do demographics questions
         continue_button.setOnClickListener(new View.OnClickListener() {
@@ -38,14 +41,12 @@ public class DemographicsIntroActivity extends AppCompatActivity {
         });
     }
 
-    // When activity is visible to user: Continue button will have width
-    // Note: Used when the text was "continue" (longer) and "skip" (shorter), and side by side
-    // buttons. Now deprecated.
-//    @Override
-//    public void onWindowFocusChanged(boolean hasFocus) {
-//        super.onWindowFocusChanged(hasFocus);
-//
-//        // Make both buttons the same width for aesthetic
-//        skip_button.setWidth(continue_button.getMeasuredWidth());
-//    }
+    // When activity is visible to user: Continue button will have same width as skip.
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        // Make both buttons the same width for aesthetic
+        continue_button.setWidth(skip_button.getMeasuredWidth());
+    }
 }
