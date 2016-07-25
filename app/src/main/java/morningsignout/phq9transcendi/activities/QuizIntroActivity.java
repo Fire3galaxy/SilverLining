bb55ab1e92cb5110a6238d03fc51a9fb23fd97ba
Iron-Sky-5 <pokeforce72@gmail.com>
2016-07-25 15:29:54 -0700

    emulator support on my comp, landscape, button style
    buttons weren't correct color in my emulator, added button style to
    make it work. also used multi-dex to let it build on my emulator. landscape
    for demographics intro improved based on small emulator.

--- app/src/main/java/morningsignout/phq9transcendi/activities/QuizIntroActivity.java
+++ app/src/main/java/morningsignout/phq9transcendi/activities/QuizIntroActivity.java
@@ -1,6 +1,7 @@
 package morningsignout.phq9transcendi.activities;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -12,12 +13,16 @@ import morningsignout.phq9transcendi.R;
  * Created by pokeforce on 5/6/16.
  */
 public class QuizIntroActivity extends AppCompatActivity {
+    final static private int PORTRAIT_HEIGHT_DP = 100, LANDSCAPE_HEIGHT_DP = 70;
+
+    Button start;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_intro);
 
-        Button start = (Button) findViewById(R.id.button_start_quiz);
+        start = (Button) findViewById(R.id.button_start_quiz);
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -26,5 +31,18 @@ public class QuizIntroActivity extends AppCompatActivity {
                 QuizIntroActivity.this.finish();
             }
         });
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        // Set height of start button based on orientation
+        final float scale = getResources().getDisplayMetrics().density; // Get the screen's density scale
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE)
+            start.setHeight((int) (PORTRAIT_HEIGHT_DP * scale + 0.5f)); // Formula to convert dp to pixels
+        else
+            start.setHeight((int) (LANDSCAPE_HEIGHT_DP * scale + 0.5f));
     }
 }
