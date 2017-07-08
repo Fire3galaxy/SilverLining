3d55a26acb1e033d4dabe297b1ddd8dcfd4c31c1
jkapich <jkapich@gmail.com>
2017-07-08 00:23:30 -0700

    notifications now still work even after phone is rebooted. Alarms disappear when app is uninstalled

--- app/src/main/java/morningsignout/phq9transcendi/activities/Activities/SettingsActivity.java
+++ app/src/main/java/morningsignout/phq9transcendi/activities/Activities/SettingsActivity.java
@@ -30,14 +30,10 @@ public class SettingsActivity extends AppCompatActivity {
     Context context;
 
     //For notifications
-    private final int ALARM_ID = 3;
-    private AlarmManager alarmMgr;
-    private PendingIntent alarmIntent;
-    String NOTIF_PREF = "Notification_Preferences";
+    final static String NOTIF_PREF = "Notification_Preferences";
 
     //for themes settings
     ListView contents;
-    Button homeButton;
     ThemesAdapter themeCustomAdapter;
 
     @Override
@@ -46,9 +42,6 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         context = this;
-
-        //----- SET UP ALARMS SETTING -----
-        alarmMgr = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
 
         //populate the spinner with frequencies of notification
         final Spinner notification_spinner = (Spinner) findViewById(R.id.notification_spinner);
@@ -70,7 +63,7 @@ public class SettingsActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 int frequency = notification_spinner.getSelectedItemPosition();
-                changeAlarmSettings(frequency);
+                NotificationReceiver.changeAlarmSettings(context, frequency);
             }
         });
 
@@ -86,14 +79,6 @@ public class SettingsActivity extends AppCompatActivity {
             }
         });
 
-        homeButton = (Button)findViewById(R.id.back_button);
-        homeButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(SettingsActivity.this, IndexActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
 
         // Create the Custom Adapter Object
         themeCustomAdapter = new ThemesAdapter(this);
@@ -102,62 +87,7 @@ public class SettingsActivity extends AppCompatActivity {
         contents.setAdapter(themeCustomAdapter);
     }
 
-    //This method changes what alarm is called.Stores preferences.
-    private void changeAlarmSettings(int frequency) {
-        //Store preferences for next time
-        SharedPreferences.Editor editor = getSharedPreferences(NOTIF_PREF, MODE_PRIVATE).edit();
-        editor.putInt("frequency", frequency);
-        editor.apply();
 
-        // it correctly shows what the user chose
-        switch (frequency) {
-            case 0: //no alarm
-                cancelAlarm(false);
-                break;
-            case 1: //weekly alarm
-                cancelAlarm(true);
-                setAlarm(AlarmManager.INTERVAL_DAY * 7);
-                Toast.makeText(context, "Weekly reminder is turned on", Toast.LENGTH_SHORT).show();
-                break;
-            case 2: //biweekly alarm
-                cancelAlarm(true);
-                setAlarm(AlarmManager.INTERVAL_DAY * 7 * 2);
-                Toast.makeText(context, "Biweekly reminder is turned on", Toast.LENGTH_SHORT).show();
-                break;
-            case 3: //tester
-                cancelAlarm(true);
-                setAlarm(1000*30);
-                Toast.makeText(context, "Tester reminder is turned on", Toast.LENGTH_SHORT).show();
-                break;
-            default:
-                break;
-        }
-
-
-    }
-
-    private void setAlarm(long frequency) {
-        Intent intent = new Intent(context, NotificationReceiver.class);
-        alarmIntent = PendingIntent.getBroadcast(context, ALARM_ID, intent, 0);
-        alarmMgr.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + frequency,
-                frequency, alarmIntent);
-
-    }
-
-
-    private void cancelAlarm(boolean switching) {
-            // If alarmIntent == null, app or activity has just been reopened.
-            // Create the alarmIntent so that it may be canceled.
-            if(alarmIntent == null) {
-                Intent intent = new Intent(context, NotificationReceiver.class);
-                alarmIntent = PendingIntent.getBroadcast(context, ALARM_ID, intent, 0);
-            }
-
-            alarmMgr.cancel(alarmIntent);
-            if (!switching) {
-                Toast.makeText(context, "Reminder is turned off", Toast.LENGTH_SHORT).show();
-            }
-    }
 
 
     @Override
--- app/src/main/java/morningsignout/phq9transcendi/activities/HelperClasses/NotificationReceiver.java
+++ app/src/main/java/morningsignout/phq9transcendi/activities/HelperClasses/NotificationReceiver.java
@@ -1,36 +1,150 @@
 package morningsignout.phq9transcendi.activities.HelperClasses;
 
+import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.PowerManager;
 import android.support.v4.app.NotificationCompat;
-import android.util.Log;
+import android.widget.Toast;
 
 import morningsignout.phq9transcendi.R;
 import morningsignout.phq9transcendi.activities.Activities.LaunchScreenActivity;
 import morningsignout.phq9transcendi.activities.Activities.SettingsActivity;
+
+import static android.content.Context.MODE_PRIVATE;
 
 /**
  * Created by jkapi on 6/24/2017.
  */
 
 public class NotificationReceiver extends BroadcastReceiver {
+    //For notifications
+    private static final int ALARM_ID = 3;
+    final static String NOTIF_PREF = "Notification_Preferences";
+
     @Override
     public void onReceive(Context context, Intent intent) {
         PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
         PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "Quiz Reminder");
         //Acquire the lock
         wl.acquire();
-        showNotification(context);
+
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            restartAlarm(context);
+
+        } else {
+            showNotification(context);
+        }
         //Release the lock
         wl.release();
     }
 
+
+    //This method changes what alarm is called.Stores preferences.
+    public static void changeAlarmSettings(Context context, int frequency) {
+        long timeSet = System.currentTimeMillis();
+        //Store preferences for next time
+        SharedPreferences.Editor editor = context.getSharedPreferences(NOTIF_PREF, MODE_PRIVATE).edit();
+        editor.putInt("frequency", frequency);
+        editor.putLong("Time set", timeSet);
+        editor.apply();
+
+        // it correctly shows what the user chose
+        switch (frequency) {
+            case 0: //no alarm
+                cancelAlarm(context, false, false);
+                break;
+            case 1: //weekly alarm
+                cancelAlarm(context, true, false);
+                setAlarm(context, AlarmManager.INTERVAL_DAY * 7, timeSet);
+                Toast.makeText(context, "Weekly reminder is turned on", Toast.LENGTH_SHORT).show();
+                break;
+            case 2: //biweekly alarm
+                cancelAlarm(context, true, false);
+                setAlarm(context, AlarmManager.INTERVAL_DAY * 7 * 2, timeSet);
+                Toast.makeText(context, "Biweekly reminder is turned on", Toast.LENGTH_SHORT).show();
+                break;
+            case 3: //tester
+                cancelAlarm(context, true, false);
+                setAlarm(context, 1000*60*2, timeSet);
+                Toast.makeText(context, "Tester reminder is turned on", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+    }
+
+    /** Name: setAlarm
+     * This method sets the notification schedule. Does not create a notification upon creation
+     * of alarm. Next notification is timeSet+frequency. Since there is only one alarm, all
+     * alarm intents are set with ALARM_ID as the id of the intent.
+     * @param context - generally going to be SettingsActivity
+     * @param frequency - frequency of Alarm.
+     * @param timeSet - when alarm was set
+     */
+    public static void setAlarm(Context context, long frequency, long timeSet) {
+        Intent intent = new Intent(context, NotificationReceiver.class);
+        PendingIntent alarmIntent = PendingIntent.getBroadcast(context, ALARM_ID, intent, 0);
+        AlarmManager alarmMgr = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+
+        alarmMgr.setRepeating(AlarmManager.RTC_WAKEUP, timeSet + frequency,
+                frequency, alarmIntent);
+
+    }
+
+    /** Name: cancelAlarm
+     * Cancels alarm if alarm exists.
+     * @param context
+     * @param switching represents if method is being called due to user switching alarm modes from
+     *                  weekly to biweekly or vice versa
+     * @param restart repesents if method is being called due to phone having been rebooted
+     */
+    private static void cancelAlarm(Context context, boolean switching, boolean restart) {
+        // If alarmIntent == null, app or activity has just been reopened.
+        // Create the alarmIntent so that it may be canceled.
+        Intent intent = new Intent(context, NotificationReceiver.class);
+        PendingIntent alarmIntent = PendingIntent.getBroadcast(context, ALARM_ID, intent, 0);
+
+        AlarmManager alarmMgr = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        alarmMgr.cancel(alarmIntent);
+        if (!switching && !restart) {
+            Toast.makeText(context, "Reminder is turned off", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private static void restartAlarm(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences(NOTIF_PREF, Context.MODE_PRIVATE);
+        int frequency = prefs.getInt("frequency", 0);
+        long timeSet = prefs.getLong("Time set",0);
+        // it correctly shows what the user chose
+        switch (frequency) {
+            case 0: //no alarm
+                cancelAlarm(context, false, true);
+                break;
+            case 1: //weekly alarm
+                cancelAlarm(context, false, true);
+                setAlarm(context, AlarmManager.INTERVAL_DAY * 7, timeSet);
+                break;
+            case 2: //biweekly alarm
+                cancelAlarm(context, false, true);
+                setAlarm(context, AlarmManager.INTERVAL_DAY * 7 * 2, timeSet);
+                break;
+            case 3: //tester
+                cancelAlarm(context, false, true);
+                setAlarm(context, 1000*60*2, timeSet);
+                break;
+            default:
+                break;
+        }
+
+    }
 
     public void showNotification(Context context) {
         int reqCode = 0; //unique number for the intent
