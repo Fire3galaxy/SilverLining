db8d735de2f80bf3938781a9e3024867868fc47e
jkapich <jkapich@gmail.com>
2017-08-02 21:19:17 -0700

    still trying to fix

--- app/src/main/java/morningsignout/phq9transcendi/activities/Activities/ResultsActivity.java
+++ app/src/main/java/morningsignout/phq9transcendi/activities/Activities/ResultsActivity.java
@@ -18,7 +18,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import morningsignout.phq9transcendi.R;
+
 import morningsignout.phq9transcendi.activities.HelperClasses.BlinkScrollView;
 import morningsignout.phq9transcendi.activities.HelperClasses.Utils;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
