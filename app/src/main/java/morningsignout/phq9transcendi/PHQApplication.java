9098a589a617f6b0d03528947e25ea1625ba889d
BreezySky <pokeforce72@gmail.com>
2017-08-29 19:15:01 -0700

    release version
    Made app not debug. Removed tester from notifications. Google-services.json for
    default firebase app not working for some reason. Just making the database
    manually for now and it works.

--- app/src/main/java/morningsignout/phq9transcendi/PHQApplication.java
+++ app/src/main/java/morningsignout/phq9transcendi/PHQApplication.java
@@ -13,7 +13,7 @@ import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
  */
 public class PHQApplication extends Application {
     private static PHQApplication singleton;
-    private static final boolean USE_DEBUG_DB = true; // See comment for getFirebaseAppInstance()
+    private static final boolean USE_DEBUG_DB = false; // See comment for getFirebaseAppInstance()
 
     public static PHQApplication getInstance() {
         return singleton;
@@ -60,7 +60,29 @@ public class PHQApplication extends Application {
             }
 
             return FirebaseApp.getInstance("DEBUG");
-        } else
-            return FirebaseApp.getInstance();
+        } else {
+            /* FIXME: For some reason, google-services.json isn't loading this database
+             * automatically. For now, just creating database from scratch.*/
+
+            // Check list of existing databases in app
+            boolean defaultExists = false;
+            for (FirebaseApp app : FirebaseApp.getApps(getInstance())) {
+                if (app.getName().equals(FirebaseApp.DEFAULT_APP_NAME)) {
+                    defaultExists = true;
+                    break;
+                }
+            }
+
+            if (!defaultExists) {
+                FirebaseOptions options = new FirebaseOptions.Builder()
+                        .setApiKey("AIzaSyAFRxgV1UO8n_4-AWrchXnup8PiUsl_qgs")
+                        .setApplicationId("1:633092681994:android:33e4a0d819b5b631")
+                        .setDatabaseUrl("https://android-phq-9-app.firebaseio.com")
+                        .build();
+                return FirebaseApp.initializeApp(getInstance(), options, FirebaseApp.DEFAULT_APP_NAME);
+            }
+
+            return FirebaseApp.getInstance(FirebaseApp.DEFAULT_APP_NAME);
+        }
     }
 }
