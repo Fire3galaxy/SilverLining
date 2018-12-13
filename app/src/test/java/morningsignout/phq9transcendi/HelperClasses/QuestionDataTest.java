9de4a6d42fe6d758379b293253b49e5fc7bb0cbd
Fire3galaxy <daniel_handojo@hotmail.com>
2018-12-12 23:44:17 -0800

    Since QuestionData uses File I/O, and that's bad for unit testing..., I've moved all questionData setup to a common setup method. Ideally, probably should be mocking the spreadsheet, but I like having a dummy spreadsheet. Dah.

--- app/src/test/java/morningsignout/phq9transcendi/HelperClasses/QuestionDataTest.java
+++ app/src/test/java/morningsignout/phq9transcendi/HelperClasses/QuestionDataTest.java
@@ -8,28 +8,30 @@ import static org.junit.jupiter.api.Assertions.*;
 // Test cases do not rely on Context class to get excel sheet from Assets folder.
 // Instead, they use a local dummy version in the root of the project.
 class QuestionDataTest {
+    private static QuestionData questionData;
+
+    @org.junit.jupiter.api.BeforeAll
+    static void setUp() {
+        questionData = new QuestionData();
+    }
+
     @org.junit.jupiter.api.Test
     void ensureDummySpreadsheetIsUsed() {
-        QuestionData questionData = new QuestionData();
-
         assertEquals(questionData.getQuestionText("DUMMY"), "DUMMY QUESTION");
     }
 
     @org.junit.jupiter.api.Test
     void loadQuestionNames_countIsNonZero() {
-        QuestionData questionData = new QuestionData();
-
         assertNotEquals(0, questionData.size(),
                 "Number of questions in spreadsheet should never be 0");
     }
 
     @org.junit.jupiter.params.ParameterizedTest
-    @CsvSource(
-            {"anhedoniainterest, Example Question 1"
+    @CsvSource({
+            "anhedoniainterest, Example Question 1",
+            "guilt, Example Question 2"
             })
     void loadQuestionNames_containsCorrectStrings(String questionName, String actualQuestionText) {
-        QuestionData questionData = new QuestionData();
-
         assertEquals(actualQuestionText, questionData.getQuestionText(questionName));
     }
 }
